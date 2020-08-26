9f18da1abcf68bcfa7ccf87dbc93ebe87b1e3821
Alex Lundquist <alex@alexlundquist.com>
2020-08-26 15:05:00 -0600

    Added Drew's model and API controller. Renamed files to "Trivia"

---
+++ DabTrivia/model/Trivia.swift
@@ -0,0 +1,19 @@
+//
+//  Trivia.swift
+//  DabTrivia
+//
+//  Created by Alex Lundquist on 8/26/20.
+//
+
+import Foundation
+
+struct TriviaTopLevelObject: Decodable {
+	let results: [Trivia]
+}
+
+struct Trivia: Decodable {
+	let difficulty: String
+	let question: String
+	let correct_answer: String
+	let incorrect_answers: [String]
+}
